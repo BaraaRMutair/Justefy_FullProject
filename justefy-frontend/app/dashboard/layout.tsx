5404b1ced1039fc163d9065c0fd7d907faf2e477
BaraaMutair <baraamutair2003@gmail.com>
2026-05-25 10:35:24 +0200

    open dashboarfd

--- justefy-frontend/app/dashboard/layout.tsx
+++ justefy-frontend/app/dashboard/layout.tsx
@@ -23,19 +23,18 @@ export default function DashboardLayout({
   useEffect(() => {
     // التحقق الآمن بعد التأكد من أن الكود يعمل بالكامل على المتصفح
 
-    setIsAuthorized(true);
-  setCheckingAuth(false);
-    // if (typeof window !== "undefined") {
-    //   const role = localStorage.getItem("user_role");
+  
+    if (typeof window !== "undefined") {
+      const role = localStorage.getItem("user_role");
       
-    //   if (role === "admin") {
-    //     setIsAuthorized(true);
-    //   } else {
-    //     setIsAuthorized(false);
-    //     router.replace("/"); // طرد المستخدم العادي للرئيسية
-    //   }
-    //   setCheckingAuth(false);
-    // }
+      if (role === "admin") {
+        setIsAuthorized(true);
+      } else {
+        setIsAuthorized(false);
+        router.replace("/"); // طرد المستخدم العادي للرئيسية
+      }
+      setCheckingAuth(false);
+    }
   }, [router]);
 
   // إذا جاري الفحص أو غير مصرح له، لا تعرض أي شيء في المتصفح لمنع الوميض أو الطردة المزدوجة
